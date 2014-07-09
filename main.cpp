3bc6e3e6c0f3d403b4c185114daebd2c69d8ae86
Edwin Loh <edwin.loh0@gmail.com>
2014-07-09 11:26:10 +0800

    Hello World

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HellowWorld
+//
+//  Created by Edwin Loh on 9/7/14.
+//  Copyright (c) 2014 Edlution Pte Ltd. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
